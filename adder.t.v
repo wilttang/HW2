867c1be2d7788a9f2c34e15b81ee4777dff0fec6
Wilson Tang <wilson.t.tang@gmail.com>
2017-09-23 23:31:12 -0400

    updated delay

--- adder.t.v
+++ adder.t.v
@@ -6,28 +6,28 @@ module testFullAdder();
     reg a, b, carryin;
     wire sum, carryout;
 
-    behavioralFullAdder adder (sum, carryout, a, b, carryin);
-    //structuralFullAdder adder (sum, carryout, a, b, carryin);
+    //behavioralFullAdder adder (sum, carryout, a, b, carryin);
+    structuralFullAdder adder (sum, carryout, a, b, carryin);
 
     initial begin
     $display("carryin|a b|sum|carryout");
 
-    carryin=0;a=0;b=0; #1000 
+    carryin=0;a=0;b=0; #50
     $display("%b   | %b  %b | %b | %b ", carryin,a,b,sum,carryout);
-    carryin=0;a=1;b=0; #1000 
+    carryin=0;a=1;b=0; #50 
     $display("%b   | %b  %b | %b | %b ", carryin,a,b,sum,carryout);
-    carryin=0;a=0;b=1; #1000 
+    carryin=0;a=0;b=1; #50 
     $display("%b   | %b  %b | %b | %b ", carryin,a,b,sum,carryout);
     carryin=0;a=1;b=1; #1000 
     $display("%b   | %b  %b | %b | %b ", carryin,a,b,sum,carryout);
  
-    carryin=1;a=0;b=0; #1000 
+    carryin=1;a=0;b=0; #50
     $display("%b   | %b  %b | %b | %b ", carryin,a,b,sum,carryout);
-    carryin=1;a=1;b=0; #1000 
+    carryin=1;a=1;b=0; #50
     $display("%b   | %b  %b | %b | %b ", carryin,a,b,sum,carryout);
-    carryin=1;a=0;b=1; #1000 
+    carryin=1;a=0;b=1; #50 
     $display("%b   | %b  %b | %b | %b ", carryin,a,b,sum,carryout);
-    carryin=1;a=1;b=1; #1000 
+    carryin=1;a=1;b=1; #50 
     $display("%b   | %b  %b | %b | %b ", carryin,a,b,sum,carryout);
 
     end
--- decoder.t.v
+++ decoder.t.v
@@ -12,21 +12,21 @@ module testDecoder ();
 
     initial begin
     $display("En A0 A1| O0 O1 O2 O3 | Expected Output");
-    enable=0;addr0=0;addr1=0; #1000 
+    enable=0;addr0=0;addr1=0; #50
     $display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
-    enable=0;addr0=1;addr1=0; #1000
+    enable=0;addr0=1;addr1=0; #50
     $display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
-    enable=0;addr0=0;addr1=1; #1000 
+    enable=0;addr0=0;addr1=1; #50 
     $display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
-    enable=0;addr0=1;addr1=1; #1000 
+    enable=0;addr0=1;addr1=1; #50
     $display("%b  %b  %b |  %b  %b  %b  %b | All false", enable, addr0, addr1, out0, out1, out2, out3);
-    enable=1;addr0=0;addr1=0; #1000 
+    enable=1;addr0=0;addr1=0; #50
     $display("%b  %b  %b |  %b  %b  %b  %b | O0 Only", enable, addr0, addr1, out0, out1, out2, out3);
-    enable=1;addr0=1;addr1=0; #1000 
+    enable=1;addr0=1;addr1=0; #50 
     $display("%b  %b  %b |  %b  %b  %b  %b | O1 Only", enable, addr0, addr1, out0, out1, out2, out3);
-    enable=1;addr0=0;addr1=1; #1000 
+    enable=1;addr0=0;addr1=1; #50
     $display("%b  %b  %b |  %b  %b  %b  %b | O2 Only", enable, addr0, addr1, out0, out1, out2, out3);
-    enable=1;addr0=1;addr1=1; #1000 
+    enable=1;addr0=1;addr1=1; #50
     $display("%b  %b  %b |  %b  %b  %b  %b | O3 Only", enable, addr0, addr1, out0, out1, out2, out3);
     end
 
